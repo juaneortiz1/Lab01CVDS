efca160a707b131e6ce685adb85b5172b895a7a0
Juanes <juan.ortiz-p@mail.escuelaing.edu.co>
2024-02-07 12:46:07 -0500

    Correccion pequeña

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeMain.java
@@ -1,19 +1,21 @@
 package edu.eci.cvds.patterns.shapes;
 
 public class ShapeMain {
-
   public static void main(String[] args) {
     if (args == null || args.length != 1) {
       System.err.println("Parameter of type RegularShapeType is required.");
       return;
     }
-
     try {
       RegularShapeType type = RegularShapeType.valueOf(args[0]);
       Shape shape = ShapeFactory.create(type);
       System.out.println(
         String.format(
-          "Successfully created a "+ type +" with "+ shape.getNumberOfEdges()
+                String.format(
+                        "Successfully created a %s with %s sides.",
+                        type,
+                        shape.getNumberOfEdges()
+                )
         )
       );
     }
@@ -22,8 +24,6 @@ public class ShapeMain {
         "Parameter '" + args[0] + "' is not a valid RegularShapeType"
       );
       return;
-
     }
-
   }
 }
